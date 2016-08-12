46d96e72be0cd60fce29188688dd0d0a36decf46
Yonatan Broza <yonatan.broza@gmail.com>
2016-08-12 22:28:34 +0300

    Added Persons for Eyal and Alon.

--- schema/create.sql
+++ schema/create.sql
@@ -8,3 +8,15 @@ INSERT INTO OnlineAccount (accountName, accountServiceHomepage) VALUES
     ('yonatan.broza', 'https://www.facebook.com/');
 CREATE EDGE account FROM (SELECT FROM Person WHERE firstName = 'Yonatan') TO
     (SELECT FROM OnlineAccount WHERE accountName = 'yonatan.broza');
+
+INSERT INTO Person (firstName) VALUES ('Eyal');
+INSERT INTO OnlineAccount (accountName, accountServiceHomepage) VALUES
+    ('pinkasey', 'https://www.facebook.com/');
+CREATE EDGE account FROM (SELECT FROM Person WHERE firstName = 'Eyal') TO
+    (SELECT FROM OnlineAccount WHERE accountName = 'pinkasey');
+
+INSERT INTO Person (firstName) VALUES ('Alon');
+INSERT INTO OnlineAccount (accountName, accountServiceHomepage) VALUES
+    ('allonr', 'https://www.facebook.com/');
+CREATE EDGE account FROM (SELECT FROM Person WHERE firstName = 'Alon') TO
+    (SELECT FROM OnlineAccount WHERE accountName = 'allonr');
