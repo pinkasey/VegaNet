8f1603daf1cec4269efc465c9b271bd248d97c73
Yonatan Broza <yonatan.broza@gmail.com>
2016-08-12 23:38:52 +0300

    Added the influencedBy edge.

--- schema/create.sql
+++ schema/create.sql
@@ -1,8 +1,10 @@
--- Creates a new database
+-- Creates classes
 CREATE CLASS Person EXTENDS V;
 CREATE CLASS OnlineAccount EXTENDS V;
 CREATE CLASS account EXTENDS E;
+CREATE CLASS influencedBy EXTENDS E;
 
+-- Creates persons
 INSERT INTO Person (firstName) VALUES ('Yonatan');
 INSERT INTO OnlineAccount (accountName, accountServiceHomepage) VALUES
     ('yonatan.broza', 'https://www.facebook.com/');
@@ -20,3 +22,11 @@ INSERT INTO OnlineAccount (accountName, accountServiceHomepage) VALUES
     ('allonr', 'https://www.facebook.com/');
 CREATE EDGE account FROM (SELECT FROM Person WHERE firstName = 'Alon') TO
     (SELECT FROM OnlineAccount WHERE accountName = 'allonr');
+
+-- Creates veganizing relationships
+CREATE EDGE influencedBy
+    FROM (SELECT FROM Person WHERE firstName = 'Yonatan')
+    TO (SELECT FROM Person WHERE firstName = 'Eyal');
+CREATE EDGE influencedBy
+    FROM (SELECT FROM Person WHERE firstName = 'Eyal')
+    TO (SELECT FROM Person WHERE firstName = 'Alon');
